c1616a7cafceef752e6b9b0eec173fb2aad3b1f0
mdshafikulislam <shafikul1170@gmail.com>
2021-08-03 01:02:39 +0600

    init project file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  node: 'jit',
+  purge: ['./*.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
